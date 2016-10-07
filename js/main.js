54527a53606840db8e184a27822fb5da977135cb
qc-z <18826255448@163.com>
2016-10-08 00:29:20 +0800

    news

--- js/main.js
+++ js/main.js
@@ -19,5 +19,33 @@ $(document).ready(function (){
        });
    }
     $(window).on("resize",resize).trigger("resize");
+
+    // 初始化tooltips插件
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+
+//    选项卡滚动
+
+
+    var $navtabs = $(".nav-tabs");
+    var width = 30; //默认有padding-left  20px；
+    $navtabs.children().each(function (i,value){
+        width += $(value).width();
+    });
+
+
+    if($(window).width() < width){
+        $navtabs.css("width",width).parent().css("overflow-x","scroll");
+    }
+
+    var $newsTitle = $(".news-title");
+    $("#news .nav-pills a").on("click mouseenter",function (){
+       var $this = $(this);
+        //console.log($this.data("title"));
+        $newsTitle.text($this.data("title"));
+
+    });
+
 })
 
